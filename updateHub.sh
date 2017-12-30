641592e810c5a516ff124884d6607db200c43e04
Sébastien Lucas <sebastien@slucas.fr>
2017-12-30 22:23:33 +0100

    Update the updater

--- updateHub.sh
+++ updateHub.sh
@@ -1,8 +1,10 @@
 #/bin/sh
 
 TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+MAJMIN=`echo $TAG | cut -d . -f 1,2 `
 
 echo $TAG
+echo $MAJMIN
 
 for dir in alpine-python3 alpine-python2 alpine-mosquitto alpine-minidlna # alpine-python3-cron
 do
@@ -11,6 +13,7 @@ do
 	docker push seblucas/$dir:armv6-$TAG
 	sed -i "s|{image}|$dir|g" ../manifest.yaml
 	sed -i "s|{tag}|$TAG|g" ../manifest.yaml
+	sed -i "s|{majmin}|$MAJMIN|g" ../manifest.yaml
 	manifest-tool push from-spec ../manifest.yaml
 	git checkout ../manifest.yaml
 	cd ..
