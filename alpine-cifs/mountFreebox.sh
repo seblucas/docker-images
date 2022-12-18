d673f842138b6ca64e3aa788eac60915609e65f0
Sébastien Lucas <sebastien@slucas.fr>
2022-12-18 21:23:41 +0100

    Update default mount option for freebox

--- alpine-cifs/mountFreebox.sh
+++ alpine-cifs/mountFreebox.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mount -t cifs "//192.168.0.254/Disque Dur" /media/freebox -o guest,iocharset=utf8
+mount -t cifs "//192.168.0.254/Disque Dur" /media/freebox -o guest,iocharset=utf8,vers=1.0
